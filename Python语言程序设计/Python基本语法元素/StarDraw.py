7de8e0e5a16218dd2589a290cd264accf499c2ba
angelra <angelra@angelrabei.local>
2020-11-27 22:18:00 +0800

    Python学习 第一天（2020--11-27）

---
+++ Python语言程序设计/Python基本语法元素/StarDraw.py
@@ -0,0 +1,10 @@
+# 五角星绘制
+
+from turtle import *
+color('red', 'blue')
+begin_fill()
+for i in range(5):
+    fd(200)
+    rt(144)
+end_fill()
+done()
--- index.py
+++
@@ -1,6 +0,0 @@
-
-l = ['eins', 'zwei', 'drei']
-# 注意 fromkeys两个参数的类型
-# 注意 formkeys的调用主体
-d = dict.fromkeys(l,'hahaha')
-print(d)
